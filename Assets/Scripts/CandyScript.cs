9492d19f8af9fd887e10533b182206cfaa1b4774
thekrocker <sdedeakay@yahoo.com>
2021-08-10 16:02:56 +0300

    Monster eats candies
    added onTrigger function to eat candies falling

---
+++ Assets/Scripts/CandyScript.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CandyScript : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            //Increment Score
+            Destroy(gameObject);
+        }
+    }
+}
