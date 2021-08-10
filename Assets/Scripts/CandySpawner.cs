f678be5debcb819726ed01faa0363c635e980f0c
thekrocker <sdedeakay@yahoo.com>
2021-08-10 16:18:33 +0300

    Spawn candy in proper position

---
+++ Assets/Scripts/CandySpawner.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using JetBrains.Annotations;
+using UnityEngine;
+
+public class CandySpawner : MonoBehaviour
+{
+    [SerializeField] private float maxX; // boundary for spawner... around -6.5x would be cool
+
+    public GameObject[] candies;
+    void Start()
+    {
+        SpawnCandy();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void SpawnCandy()
+    {
+        int rand = Random.Range(0, candies.Length);
+
+        
+        Instantiate(candies[rand], transform.position, transform.rotation);
+    }
+}
