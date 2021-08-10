44332edf9248bc5a4c94e35ca0ca2f8c7ed6ee55
thekrocker <sdedeakay@yahoo.com>
2021-08-10 16:31:59 +0300

    added candy spawner
    created candy spawner with random.range and coroutines

--- Assets/Scripts/CandySpawner.cs
+++ Assets/Scripts/CandySpawner.cs
@@ -1,16 +1,31 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using JetBrains.Annotations;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class CandySpawner : MonoBehaviour
 {
     [SerializeField] private float maxX; // boundary for spawner... around -6.5x would be cool
 
     public GameObject[] candies;
+
+    [SerializeField] private float spawnInterval;
+
+    public static CandySpawner instance;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+    }
+
     void Start()
     {
-        SpawnCandy();
+        StartSpawningCandies();
     }
 
     // Update is called once per frame
@@ -22,8 +37,36 @@ public class CandySpawner : MonoBehaviour
     void SpawnCandy()
     {
         int rand = Random.Range(0, candies.Length);
-
         
-        Instantiate(candies[rand], transform.position, transform.rotation);
+        float randX = Random.Range(-maxX, maxX); // randomly gives value of candies between boundaries in X axis.
+        
+        Vector3 randomPos = new Vector3(randX, transform.position.y, transform.position.z);
+        
+        Instantiate(candies[rand],randomPos, transform.rotation); // Instantiating candies at random position
     }
+
+    IEnumerator SpawnCandies()
+    {
+        yield return new WaitForSeconds(2f); // wait 2 secs to respawn 
+        
+        while (true)
+        {
+            SpawnCandy();
+
+            yield return new WaitForSeconds(spawnInterval);
+        }
+        
+    }
+
+    public void StartSpawningCandies()
+    {
+        StartCoroutine(SpawnCandies());
+    }
+
+    public void StopSpawningCandies()
+    {
+        StopCoroutine(SpawnCandies());
+    }
+    
+    
 }
